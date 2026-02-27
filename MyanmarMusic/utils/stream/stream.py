043a2f55dc774f59ee9438b4e7bf73f67b78166e
Telegram & App source Code <khithlainhtet2021@gmail.com>
2026-02-27 11:40:06 +0630

    MYANMARMUSIC

---
+++ MyanmarMusic/utils/stream/stream.py
@@ -0,0 +1,465 @@
+import os
+from random import randint
+from typing import Union
+
+from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.enums import ParseMode
+
+import config
+from MyanmarMusic import Carbon, YouTube, app
+from MyanmarMusic.core.call import Hotty
+from MyanmarMusic.misc import db
+from MyanmarMusic.utils.database import add_active_video_chat, is_active_chat
+from MyanmarMusic.utils.exceptions import AssistantErr
+from MyanmarMusic.utils.inline import aq_markup, close_markup, stream_markup
+from MyanmarMusic.utils.pastebin import HottyBin
+from MyanmarMusic.utils.stream.queue import put_queue, put_queue_index
+from MyanmarMusic.utils.thumbnails import get_thumb
+
+
+async def stream(
+    _,
+    mystic,
+    user_id,
+    result,
+    chat_id,
+    user_name,
+    original_chat_id,
+    video: Union[bool, str] = None,
+    streamtype: Union[bool, str] = None,
+    spotify: Union[bool, str] = None,
+    forceplay: Union[bool, str] = None,
+):
+    if not result:
+        return
+    if forceplay:
+        await Hotty.force_stop_stream(chat_id)
+
+    # 🟢 [PREMIUM EMOJI - SAFE HTML ဖြင့် ပြင်ဆင်ချက်] 🟢
+    def get_stream_text(t, d, u):
+        # ValueError မတက်စေရန် အထူးသင်္ကေတများကို ပြင်ဆင်ခြင်း
+        t = str(t).replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
+        u = str(u).replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
+        
+        return (
+            f"<blockquote><emoji id='6120630771087579219'>🐼</emoji> <b>စတင်ထုတ်လွှင့်နေပြီ</b> |</blockquote>\n"
+            f"<blockquote><emoji id='6120465303177533732'>🐼</emoji> <b>ခေါင်းစဉ် :</b> {t[:27]}\n"
+            f"<emoji id='6120591326107935086'>🐼</emoji> <b>ကြာချိန် :</b> {d} ᴍɪɴᴜᴛᴇs\n"
+            f"<emoji id='6120398056874582504'>🐼</emoji> <b>တောင်းဆိုသူ :</b> {u}\n\n🐼MADE by🌻 HAN THAR</blockquote>"
+        )
+        
+    def get_queue_text(t, d, u, p):
+        t = str(t).replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
+        u = str(u).replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
+        
+        return (
+            f"<blockquote><emoji id='5895705279416241926'>🐼</emoji> <b>စာရင်းထဲသို့ ထည့်သွင်းလိုက်ပါပြီ</b> |</blockquote>\n"
+            f"<blockquote><emoji id='5962964097904413958'>🐼</emoji> <b>ခေါင်းစဉ် :</b> {t[:27]}\n"
+            f"<emoji id='5960715046869798071'>⏱</emoji> <b>ကြာချိန် :</b> {d} ᴍɪɴᴜᴛᴇs\n"
+            f"<emoji id='6325666711592769876'>🐼</emoji> <b>တောင်းဆိုသူ :</b> {u}\n"
+            f"<emoji id='5960863197471706506'>🐼</emoji> <b>အမှတ်စဉ် :</b> {p}\n\n🐼MADE BY 🤍 HAN THAR</blockquote>"
+        )
+    # -----------------------------------------------------------
+
+    if streamtype == "playlist":
+        msg = f"{_['play_19']}\n\n"
+        count = 0
+        for search in result:
+            if int(count) == config.PLAYLIST_FETCH_LIMIT:
+                continue
+            try:
+                (
+                    title,
+                    duration_min,
+                    duration_sec,
+                    thumbnail,
+                    vidid,
+                ) = await YouTube.details(search, False if spotify else True)
+            except:
+                continue
+            if str(duration_min) == "None":
+                continue
+            if duration_sec > config.DURATION_LIMIT:
+                continue
+            if await is_active_chat(chat_id):
+                await put_queue(
+                    chat_id,
+                    original_chat_id,
+                    f"vid_{vidid}",
+                    title,
+                    duration_min,
+                    user_name,
+                    vidid,
+                    user_id,
+                    "video" if video else "audio",
+                )
+                position = len(db.get(chat_id)) - 1
+                count += 1
+                msg += f"{count}. {title[:70]}\n"
+                msg += f"{_['play_20']} {position}\n\n"
+            else:
+                if not forceplay:
+                    db[chat_id] = []
+                status = True if video else None
+
+                try:
+                    await mystic.edit_text(_["play_dl"].format(title))
+                except KeyError:
+                    await mystic.edit_text(f"Dow͟n͟l͟o͟a͟d͟ ဆွဲနေပါသည် 🤍 ᥫ᭡ {title}")
+
+                try:
+                    file_path, direct = await YouTube.download(
+                        vidid, mystic, video=status, videoid=True
+                    )
+                except:
+                    raise AssistantErr(_["play_14"])
+                await Hotty.join_call(
+                    chat_id,
+                    original_chat_id,
+                    file_path,
+                    video=status,
+                    image=thumbnail,
+                )
+                await put_queue(
+                    chat_id,
+                    original_chat_id,
+                    file_path if direct else f"vid_{vidid}",
+                    title,
+                    duration_min,
+                    user_name,
+                    vidid,
+                    user_id,
+                    "video" if video else "audio",
+                    forceplay=forceplay,
+                )
+                img = await get_thumb(vidid)
+                button = stream_markup(_, chat_id)
+                run = await app.send_photo(
+                    original_chat_id,
+                    photo=img,
+                    caption=get_stream_text(title, duration_min, user_name),
+                    reply_markup=InlineKeyboardMarkup(button),
+                    parse_mode=ParseMode.HTML,
+                )
+                db[chat_id][0]["mystic"] = run
+                db[chat_id][0]["markup"] = "stream"
+        if count == 0:
+            return
+        else:
+            link = await HottyBin(msg)
+            lines = msg.count("\n")
+            if lines >= 17:
+                car = os.linesep.join(msg.split(os.linesep)[:17])
+            else:
+                car = msg
+            carbon = await Carbon.generate(car, randint(100, 10000000))
+            upl = close_markup(_)
+            return await app.send_photo(
+                original_chat_id,
+                photo=carbon,
+                caption=_["play_21"].format(position, link),
+                reply_markup=upl,
+            )
+    elif streamtype == "youtube":
+        link = result["link"]
+        vidid = result["vidid"]
+        title = (result["title"]).title()
+        duration_min = result["duration_min"]
+        thumbnail = result["thumb"]
+        status = True if video else None
+
+        current_queue = db.get(chat_id)
+
+        if current_queue is not None and len(current_queue) >= 50:
+            return await app.send_message(original_chat_id, "You can't add more than 50 songs to the queue.")
+
+        try:
+            await mystic.edit_text(_["play_dl"].format(title))
+        except KeyError:
+            await mystic.edit_text(f"Dow͟n͟l͟o͟a͟d͟ ဆွဲနေပါသည် 🤍 ᥫ᭡ {title}")
+
+        try:
+            file_path, direct = await YouTube.download(
+                vidid, mystic, videoid=True, video=status
+            )
+        except:
+            raise AssistantErr(_["play_14"])
+
+        if await is_active_chat(chat_id):
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path,  
+                title,
+                duration_min,
+                user_name,
+                vidid,
+                user_id,
+                "video" if video else "audio",
+            )
+            position = len(db.get(chat_id)) - 1
+            button = aq_markup(_, chat_id)
+            await app.send_message(
+                chat_id=original_chat_id,
+                text=get_queue_text(title, duration_min, user_name, position),
+                reply_markup=InlineKeyboardMarkup(button),
+                parse_mode=ParseMode.HTML,
+            )
+        else:
+            if not forceplay:
+                db[chat_id] = []
+            await Hotty.join_call(
+                chat_id,
+                original_chat_id,
+                file_path,
+                video=status,
+                image=thumbnail,
+            )
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path, 
+                title,
+                duration_min,
+                user_name,
+                vidid,
+                user_id,
+                "video" if video else "audio",
+                forceplay=forceplay,
+            )
+            img = await get_thumb(vidid)
+            button = stream_markup(_, chat_id)
+            run = await app.send_photo(
+                original_chat_id,
+                photo=img,
+                caption=get_stream_text(title, duration_min, user_name),
+                reply_markup=InlineKeyboardMarkup(button),
+                parse_mode=ParseMode.HTML,
+            )
+            db[chat_id][0]["mystic"] = run
+            db[chat_id][0]["markup"] = "stream"
+    elif streamtype == "soundcloud":
+        file_path = result["filepath"]
+        title = result["title"]
+        duration_min = result["duration_min"]
+        if await is_active_chat(chat_id):
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path,
+                title,
+                duration_min,
+                user_name,
+                streamtype,
+                user_id,
+                "audio",
+            )
+            position = len(db.get(chat_id)) - 1
+            button = aq_markup(_, chat_id)
+            await app.send_message(
+                chat_id=original_chat_id,
+                text=get_queue_text(title, duration_min, user_name, position),
+                reply_markup=InlineKeyboardMarkup(button),
+                parse_mode=ParseMode.HTML,
+            )
+        else:
+            if not forceplay:
+                db[chat_id] = []
+            await Hotty.join_call(chat_id, original_chat_id, file_path, video=None)
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path,
+                title,
+                duration_min,
+                user_name,
+                streamtype,
+                user_id,
+                "audio",
+                forceplay=forceplay,
+            )
+            button = stream_markup(_, chat_id)
+            run = await app.send_photo(
+                original_chat_id,
+                photo=config.SOUNCLOUD_IMG_URL,
+                caption=get_stream_text(title, duration_min, user_name),
+                reply_markup=InlineKeyboardMarkup(button),
+                parse_mode=ParseMode.HTML,
+            )
+            db[chat_id][0]["mystic"] = run
+            db[chat_id][0]["markup"] = "tg"
+    elif streamtype == "telegram":
+        file_path = result["path"]
+        link = result["link"]
+        title = (result["title"]).title()
+        duration_min = result["dur"]
+        status = True if video else None
+        if await is_active_chat(chat_id):
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path,
+                title,
+                duration_min,
+                user_name,
+                streamtype,
+                user_id,
+                "video" if video else "audio",
+            )
+            position = len(db.get(chat_id)) - 1
+            button = aq_markup(_, chat_id)
+            await app.send_message(
+                chat_id=original_chat_id,
+                text=get_queue_text(title, duration_min, user_name, position),
+                reply_markup=InlineKeyboardMarkup(button),
+                parse_mode=ParseMode.HTML,
+            )
+        else:
+            if not forceplay:
+                db[chat_id] = []
+            await Hotty.join_call(chat_id, original_chat_id, file_path, video=status)
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path,
+                title,
+                duration_min,
+                user_name,
+                streamtype,
+                user_id,
+                "video" if video else "audio",
+                forceplay=forceplay,
+            )
+            if video:
+                await add_active_video_chat(chat_id)
+            button = stream_markup(_, chat_id)
+            run = await app.send_photo(
+                original_chat_id,
+                photo=config.TELEGRAM_VIDEO_URL if video else config.TELEGRAM_AUDIO_URL,
+                caption=get_stream_text(title, duration_min, user_name),
+                reply_markup=InlineKeyboardMarkup(button),
+                parse_mode=ParseMode.HTML,
+            )
+            db[chat_id][0]["mystic"] = run
+            db[chat_id][0]["markup"] = "tg"
+    elif streamtype == "live":
+        link = result["link"]
+        vidid = result["vidid"]
+        title = (result["title"]).title()
+        thumbnail = result["thumb"]
+        duration_min = "Live Track"
+        status = True if video else None
+        if await is_active_chat(chat_id):
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                f"live_{vidid}",
+                title,
+                duration_min,
+                user_name,
+                vidid,
+                user_id,
+                "video" if video else "audio",
+            )
+            position = len(db.get(chat_id)) - 1
+            button = aq_markup(_, chat_id)
+            await app.send_message(
+                chat_id=original_chat_id,
+                text=get_queue_text(title, duration_min, user_name, position),
+                reply_markup=InlineKeyboardMarkup(button),
+                parse_mode=ParseMode.HTML,
+            )
+        else:
+            if not forceplay:
+                db[chat_id] = []
+            n, file_path = await YouTube.video(link)
+            if n == 0:
+                raise AssistantErr(_["str_3"])
+            await Hotty.join_call(
+                chat_id,
+                original_chat_id,
+                file_path,
+                video=status,
+                image=thumbnail if thumbnail else None,
+            )
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                f"live_{vidid}",
+                title,
+                duration_min,
+                user_name,
+                vidid,
+                user_id,
+                "video" if video else "audio",
+                forceplay=forceplay,
+            )
+            img = await get_thumb(vidid)
+            button = stream_markup(_, chat_id)
+            run = await app.send_photo(
+                original_chat_id,
+                photo=img,
+                caption=get_stream_text(title, duration_min, user_name),
+                reply_markup=InlineKeyboardMarkup(button),
+                parse_mode=ParseMode.HTML,
+            )
+            db[chat_id][0]["mystic"] = run
+            db[chat_id][0]["markup"] = "tg"
+    elif streamtype == "index":
+        link = result
+        title = "ɪɴᴅᴇx ᴏʀ ᴍ3ᴜ8 ʟɪɴᴋ"
+        duration_min = "00:00"
+        if await is_active_chat(chat_id):
+            await put_queue_index(
+                chat_id,
+                original_chat_id,
+                "index_url",
+                title,
+                duration_min,
+                user_name,
+                link,
+                "video" if video else "audio",
+            )
+            position = len(db.get(chat_id)) - 1
+            button = aq_markup(_, chat_id)
+            await mystic.edit_text(
+                text=get_queue_text(title, duration_min, user_name, position),
+                reply_markup=InlineKeyboardMarkup(button),
+                parse_mode=ParseMode.HTML,
+            )
+        else:
+            if not forceplay:
+                db[chat_id] = []
+            await Hotty.join_call(
+                chat_id,
+                original_chat_id,
+                link,
+                video=True if video else None,
+            )
+            await put_queue_index(
+                chat_id,
+                original_chat_id,
+                "index_url",
+                title,
+                duration_min,
+                user_name,
+                link,
+                "video" if video else "audio",
+                forceplay=forceplay,
+            )
+            button = stream_markup(_, chat_id)
+            
+            index_text = (
+                f"<emoji id='6120465303177533732'>🐼</emoji> <b>စတင်ထုတ်လွှင့်နေပြီ (Index Stream)</b> |\n\n"
+                f"<emoji id='6120591326107935086'>🐼</emoji> <b>တောင်းဆိုသူ :</b> {user_name}"
+            )
+            run = await app.send_photo(
+                original_chat_id,
+                photo=config.STREAM_IMG_URL,
+                caption=index_text,
+                reply_markup=InlineKeyboardMarkup(button),
+                parse_mode=ParseMode.HTML,
+            )
+            db[chat_id][0]["mystic"] = run
+            db[chat_id][0]["markup"] = "tg"
+            await mystic.delete()
